605385a1ba376b0d71233f9b6044d1e09b386730
Kultus01 <mikkijett86@gmail.com>
2018-06-06 15:28:56 +0800

    install.sh
    Simple change to luckybit install guide for pre established servers, does not create swapfile during masternode installation

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo touch /var/swap.img
+#sudo touch /var/swap.img
 
-sudo chmod 600 /var/swap.img
+#sudo chmod 600 /var/swap.img
 
-sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+#sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
 
-mkswap /var/swap.img
+#mkswap /var/swap.img
 
-sudo swapon /var/swap.img
+#sudo swapon /var/swap.img
 
-sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+#sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 
 sudo apt-get update -y
 
